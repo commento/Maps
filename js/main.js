a92fe350134d74e1d4ccf0ad56d6f3a24a069b7f
commento <riccardo@null.net>
2017-05-03 22:29:12 +0200

    Added click marker color change

--- js/main.js
+++ js/main.js
@@ -12,12 +12,10 @@
 
       var drawingManager = null;
 
+      var defaultIcon = null;
+
       function initMap() {
         // Create a styles array to use with the map.
-
-
-        //var styledMapType = new google.maps.StyledMapType(
-        //[{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"},{"weight":"0.20"},{"lightness":"28"},{"saturation":"23"},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#494949"},{"lightness":13},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}], {name: 'Styled Map'});
 
         // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
@@ -30,9 +28,6 @@
           },
           mapTypeControl: false
         });
-
-        //map.mapTypes.set('styled_map', styledMapType);
-        //map.setMapTypeId('styled_map');
 
         // This autocomplete is for use in the search within time entry box.
         var timeAutocomplete = new google.maps.places.Autocomplete(
@@ -81,11 +76,9 @@
         });
 
         // Style the markers a bit. This will be our listing marker icon.
-        var defaultIcon = makeMarkerIcon('0091ff');
+        defaultIcon = makeMarkerIcon('0091ff');
 
-        // Create a "highlighted location" marker color for when the user
-        // mouses over the marker.
-        var highlightedIcon = makeMarkerIcon('144b53');
+        var clickedIcon = makeMarkerIcon('FFFFFF');
 
         // The following group uses the location array to create an array of markers on initialize.
         for (var i = 0; i < locations.length; i++) {
@@ -104,15 +97,11 @@
           markers.push(marker);
           // Create an onclick event to open the large infowindow at each marker.
           marker.addListener('click', function() {
+            for (var i = 0; i < markers.length; i++) {
+              markers[i].setIcon(defaultIcon);
+            }
+            this.setIcon(clickedIcon);
             populateInfoWindow(this, largeInfowindow);
-          });
-          // Two event listeners - one for mouseover, one for mouseout,
-          // to change the colors back and forth.
-          marker.addListener('mouseover', function() {
-            this.setIcon(highlightedIcon);
-          });
-          marker.addListener('mouseout', function() {
-            this.setIcon(defaultIcon);
           });
         }
 
@@ -161,6 +150,7 @@
           // Make sure the marker property is cleared if the infowindow is closed.
           infowindow.addListener('closeclick', function() {
             infowindow.marker = null;
+            marker.setIcon(defaultIcon);
           });
           var streetViewService = new google.maps.StreetViewService();
           var radius = 50;
@@ -209,6 +199,7 @@
           for (var i = 0; i < markers.length; i++) {
             markers[i].setMap(map);
             bounds.extend(markers[i].position);
+            //markers[i].setIcon(defaultIcon);
           }
           map.fitBounds(bounds);
 
